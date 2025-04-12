e302f9132a0be8102ce0598bbcb9672a6c713dfc
Gokul teja <147913925+gokulteja49@users.noreply.github.com>
2025-04-12 13:21:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-  origin: 'https://your-frontend-domain.com', // Allow only your frontend domain
+  origin: 'https://jobsxperts.com/', // Allow only your frontend domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 }));
